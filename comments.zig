b1103dd44b2ebe9c2e9de3ebe5003a87144cba3c
Alvin Null <supdrewin@outlook.com>
2025-12-14 22:52:43 -0500

    Comments

---
+++ comments.zig
@@ -0,0 +1,10 @@
+const print = @import("std").debug.print;
+
+pub fn main() void {
+    // Comments in Zig start with "//" and end at the next LF byte (end of line).
+    // The line below is a comment and won't be exeuted.
+
+    //print("hello?", .{});
+
+    print("Hello, World!\n", .{}); // another comment
+}
