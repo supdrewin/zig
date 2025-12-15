504a5edb092e557408d8c66cb09db3e97b4efae1
Alvin Null <supdrewin@outlook.com>
2025-12-14 23:02:06 -0500

    Doc Comments

--- comments.zig
+++ comments.zig
@@ -4,7 +4,7 @@ pub fn main() void {
     // Comments in Zig start with "//" and end at the next LF byte (end of line).
     // The line below is a comment and won't be exeuted.
 
-    //print("hello?", .{});
+    // print("hello?", .{});
 
     print("Hello, World!\n", .{}); // another comment
 }
