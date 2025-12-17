729f474b75a79ee1dd246601e280d6f5d6800dbe
Alvin Null <supdrewin@outlook.com>
2025-12-17 07:44:32 -0500

    Report Memory Leaks

---
+++ testing_detect_leak.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+test "detect leak" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(u21) = .empty;
+    // missing `defer list.deinit(gpa);`
+    try list.append(gpa, '☔');
+
+    try std.testing.expect(list.items.len == 1);
+}
